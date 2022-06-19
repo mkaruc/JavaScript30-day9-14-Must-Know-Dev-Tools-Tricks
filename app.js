88bcc8d6fca3bab2624e9794b8f2c88b06d3b9b8
mkaruc <fatihmehmetkaruc@gmail.com>
2022-06-19 22:08:26 +0300

    14-tips

---
+++ app.js
@@ -0,0 +1,25 @@
+function makeGreen() {
+    const p=document.querySelector('p');
+    p.style.color='green';
+    p.style.fontSize='3rem';
+}
+
+const dogs=[{name:'Snickers',age:2},{name:'hugo',age:8}];
+
+//regular
+console.log('hello!..');
+
+//interpolated
+console.log('Hello I am a %s engineer','electronics');
+
+//Styled
+console.log('%c I am some great text','font-size:50px');
+//warning!..
+
+//Error:|
+
+//info
+
+//Testing
+
+//clearing
